2c63b92ba432e17f653f0cb98476e9e0a0dd2d23
DHG <duyhung8a2@gmail.com>
2024-04-26 00:23:45 +0700

    feat: index user in es

--- scripts/initES.ts
+++ scripts/initES.ts
@@ -1,44 +1,88 @@
 import { Client } from "@elastic/elasticsearch";
 import { readFileSync } from "fs";
-import { Post } from "./types";
+import { Post, User } from "./types";
 import { pick } from "radash";
 
 const ITEM_PER_BULK = 5000;
-const POSTS_INDEX_NAME = "post"
+const POSTS_INDEX_NAME = "post";
+const USERS_INDEX_NAME = "user";
 
 const client = new Client({
   node: "http://localhost:9200",
 });
 
-const jsonData = readFileSync("dataset/posts.json", "utf-8");
-const posts: Post[] = JSON.parse(jsonData);
+const postJsonData = readFileSync("dataset/posts.json", "utf-8");
+const posts: Post[] = JSON.parse(postJsonData);
 let postsLength = posts.length;
 
-async function indexPosts() {
+const userJsonData = readFileSync("dataset/users.json", "utf-8");
+const users: User[] = JSON.parse(userJsonData);
+let usersLength = users.length;
+
+async function indexPosts(
+  indexName = POSTS_INDEX_NAME,
+  itemPerBulk = ITEM_PER_BULK,
+  itemsLength = postsLength
+) {
   console.log("Ready to index");
-  console.time("index");
+  console.time("index-post");
   let bulkBody: any[] = [];
 
   for (const post of posts) {
     const doc = pick(post, ["id", "title", "content"]);
 
-    bulkBody.push({ index: { _index: POSTS_INDEX_NAME, _id: post.id.toString() } });
+    bulkBody.push({
+      index: { _index: indexName, _id: post.id.toString() },
+    });
     bulkBody.push(doc);
 
-    if (bulkBody.length > ITEM_PER_BULK * 2) {
-      console.log("Records left: ", postsLength);
+    if (bulkBody.length > itemPerBulk * 2) {
+      console.log("Records left: ", itemsLength);
       await client.bulk({ operations: bulkBody, refresh: false });
-      postsLength -= bulkBody.length / 2;
+      itemsLength -= bulkBody.length / 2;
       bulkBody = [];
     }
   }
   if (bulkBody.length > 0) {
-    console.log("Records left: ", postsLength);
+    console.log("Records left: ", itemsLength);
     await client.bulk({ body: bulkBody });
   }
-  console.timeEnd("index");
+  console.timeEnd("index-post");
 
-  await client.indices.refresh({ index: POSTS_INDEX_NAME });
+  await client.indices.refresh({ index: indexName });
+}
+
+async function indexUsers(
+  indexName = USERS_INDEX_NAME,
+  itemPerBulk = ITEM_PER_BULK,
+  itemsLength = usersLength
+) {
+  console.time("index-user");
+  let bulkBody: any[] = [];
+
+  for (const user of users) {
+    const doc = pick(user, ["id", "name", "phone", "email"]);
+    console.log(doc);
+
+    bulkBody.push({
+      index: { _index: indexName, _id: user.id.toString() },
+    });
+    bulkBody.push(doc);
+
+    if (bulkBody.length > itemPerBulk * 2) {
+      console.log("Records left: ", itemsLength);
+      await client.bulk({ operations: bulkBody, refresh: false });
+      itemsLength -= bulkBody.length / 2;
+      bulkBody = [];
+    }
+  }
+  if (bulkBody.length > 0) {
+    console.log("Records left: ", itemsLength);
+    await client.bulk({ body: bulkBody });
+  }
+  console.timeEnd("index-user");
+
+  await client.indices.refresh({ index: indexName });
 }
 
 indexPosts()
@@ -47,4 +91,12 @@ indexPosts()
   })
   .catch((error) => {
     console.error("Error indexing posts:", error);
+  });
+
+indexUsers()
+  .then(() => {
+    console.log("Users indexed successfully");
+  })
+  .catch((error) => {
+    console.error("Error indexing users:", error);
   });
--- scripts/types.ts
+++ scripts/types.ts
@@ -11,3 +11,22 @@ export interface Post {
     update_at: Date;
     deleted: boolean;
   }
+
+  export interface User {
+  id:         number;
+  address:    string;
+  name:       string;
+  avatar:     string;
+  background: null;
+  role:       string;
+  phone:      string;
+  birthday:   Date;
+  email:      string;
+  ssn:        string;
+  sex:        string;
+  user_uuid:  string;
+  is_famous:  boolean;
+  create_at:  Date;
+  update_at:  Date;
+  deleted:    boolean;
+}
